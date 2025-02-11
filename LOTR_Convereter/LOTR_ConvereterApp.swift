b146aa9d354701d38c2e2a5ceb37f2e71e266a9f
Ichahal <83983971+Ichahal@users.noreply.github.com>
2025-02-11 11:52:55 -0500

    Initial Commit

---
+++ LOTR_Convereter/LOTR_ConvereterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConvereterApp.swift
+//  LOTR_Convereter
+//
+//  Created by Aman  Chahal on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConvereterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
